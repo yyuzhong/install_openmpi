4e943d61ccb79b91458e24a2c07d70d6b74825f0
root <yyuzhong@hotmail.com>
2015-05-20 11:58:03 -0500

    [YZ] backup

--- install_mpi.sh
+++ install_mpi.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-cat ./env_openmpi.conf >> /etc/profile
-source /etc/profile
+yum install gcc-c++
+
+cd /tmp/
+#cat ./env_openmpi.conf >> /etc/profile
+#source /etc/profile
 
 cd /opt/cscloud/
+rm -rf openmpi
 mkdir openmpi
 cd openmpi
-wget http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.3.tar.gz
+#wget http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.3.tar.gz
+scp 129.207.46.224:/home1/yzyan/openmpi/install_openmpi/openmpi-1.8.3.tar.gz ./
 tar zxvf openmpi-1.8.3.tar.gz
 
 cd openmpi-1.8.3
